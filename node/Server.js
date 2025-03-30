362e94d2520a877bb405b816a7b7ea3947ebe52a
zaman-555 <mohammadzamanfi@gmail.com>
2025-03-30 04:28:32 +0300

    updated

--- node/Server.js
+++ node/Server.js
@@ -4,7 +4,7 @@ const port = 3311;
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Node App is Running!');
+    res.end('Node App is Running');
 })
 
 server.listen(port, hostname, () => {
